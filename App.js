bae60d376f3242b56bf1088f9a4e630af582e349
Asmatchd <Asmatchd@gmail.com>
2020-07-15 18:19:49 +0500

    navigation installed

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+const Stack = createStackNavigator();
 
 import {Basics} from './src/Basics';
 import {InputField} from './src/InputField';
 
 const App = () => {
-  return <InputField />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="InputField"
+          component={InputField}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Basics"
+          component={Basics}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
