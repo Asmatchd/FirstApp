c17275bc097c3aba91e47c1896bfb81705be481f
Asmatchd <Asmatchd@gmail.com>
2020-07-21 18:04:17 +0500

    advanced textinout

--- App.js
+++ App.js
@@ -6,11 +6,17 @@ const Stack = createStackNavigator();
 
 import {Basics} from './src/Basics';
 import {InputField} from './src/InputField';
+import {SignIn} from './src/screens/signIn';
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{headerShown: false}}
+        />
         <Stack.Screen
           name="InputField"
           component={InputField}
